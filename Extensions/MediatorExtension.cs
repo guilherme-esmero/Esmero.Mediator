57ddfb347bb84493b72952de017591aff0ec5df7
Guilherme Esmero <guiesmero14@gmail.com>
2025-08-08 20:48:51 -0300

    Added implementations

---
+++ Extensions/MediatorExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Esmero.Mediator.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Esmero.Mediator.Extensions
+{
+    public static class MediatorExtension
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddTransient<IMediator, Mediator>();
+
+            var handlerInterfaceType = typeof(IHandler<,>);
+
+            foreach (var assembly in assemblies)
+            {
+                var handlers = assembly
+                    .GetTypes()
+                    .Where(t => !t.IsAbstract && !t.IsInterface)
+                    .SelectMany(t => t.GetInterfaces(), (t, i) => new { Type = t, Interface = i })
+                    .Where(ti => ti.Interface.IsGenericType && ti.Interface.GetGenericTypeDefinition() == handlerInterfaceType);
+
+                foreach (var handler in handlers)
+                    services.AddTransient(handler.Interface, handler.Type);
+            }
+
+            return services;
+        }
+    }
+}
---
+++ Mediator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esmero.Mediator.Abstractions;
+
+namespace Esmero.Mediator
+{
+    public class Mediator(IServiceProvider serviceProvider) : IMediator
+    {
+        public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request,
+            CancellationToken cancellationToken = default)
+        {
+            var requestType = request.GetType();
+            var handlerType = typeof(IHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+
+            var handlerObj = serviceProvider.GetService(handlerType);
+            if (handlerObj == null)
+                throw new InvalidOperationException($"Handler not found for {requestType.Name}");
+
+            var method = handlerType.GetMethod("HandleAsync");
+            if (method == null)
+                throw new InvalidOperationException($"HandleAsync method not found in {handlerType.Name}");
+
+            if (method.Invoke(handlerObj, [request, cancellationToken]) is not Task<TResponse> task)
+                throw new InvalidOperationException(
+                    $"Handler {handlerObj.GetType().Name} returned null or an incompatible type.");
+
+            return await task;
+        }
+    }
+}
